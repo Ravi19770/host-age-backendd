67f69643ffc494db34f89b5d8d218a1ff7337c6d
Ravi19770 <upadhyayabhishek993@gmail.com>
2026-09-08 14:15:27 +0530

    Fix production CORS

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:3001",
   "http://127.0.0.1:3002",
+
+   // Production
+  "https://host-age.in",
+  "https://www.host-age.in",
 ];
 
 app.use(
